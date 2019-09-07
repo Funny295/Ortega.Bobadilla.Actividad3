ec8fe8186e51c32df652f3a73b8ede417433e0f4
Ortega.Bobadilla.Actividad3 <stephannie.bobadilla@alumnos.udg.mx>
2019-09-06 21:46:32 -0500

    Login

---
+++ app/src/main/java/bobadilla/stephannie/bobadilla/MainActivity.java
@@ -0,0 +1,50 @@
+package bobadilla.stephannie.bobadilla;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextNombre, editTextPass;
+    Button botonIngreso;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //match con la vista
+        setContentView(R.layout.activity_main);
+        //match de los widgets
+        editTextNombre = findViewById(R.id.etNombre);
+        editTextPass = findViewById(R.id.etPass);
+        botonIngreso = findViewById(R.id.btnEntrar);
+
+        botonIngreso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombreUsuario = editTextNombre.getText().toString();
+                String password = editTextPass.getText().toString();
+
+                if (nombreUsuario.isEmpty() || password.isEmpty()) {
+                    //nombre de usuario vacio
+                    mensajito("El usuario o contraseña estan vacios");
+                } else {
+                    if (nombreUsuario.equals("stephannie") && password.equals("1234")) {
+                        //imprimir bienvenido
+                        mensajito("Bienvenido!!!");
+                    } else {
+                        //imprimir datos erroneos
+                    }
+                }
+            }
+        });
+    }
+    public void mensajito(String mensaje){
+        Toast.makeText( this,mensaje,Toast.LENGTH_LONG).show();
+    }
+
+}
